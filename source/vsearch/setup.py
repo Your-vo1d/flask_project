640d43c1ef2b8a4d0d710ae9b9f7d6ab04f249e2
Your-vo1d <my.vo1d.edge@gmail.com>
2024-06-02 19:25:25 +0700

    Created model

---
+++ source/vsearch/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="vsearch",
+    version="1.0",
+    description="Search Tools",
+    author="HF Python 2e",
+    author_email="my.vo1d.edge@gmail.com",
+    url="temp.com",
+    py_modules=["vsearch"],
+)
